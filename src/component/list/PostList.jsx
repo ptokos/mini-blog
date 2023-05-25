913c82a825df4625a2f939239b362acccda16cc9
ptokos <jiseokchoij@gmail.com>
2023-05-25 15:07:15 +0900

    feat: 글 목록에 해당하는 PostList 컴포넌트 구현

---
+++ src/component/list/PostList.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+    
+    displayL flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+ 
+`;
+
+
+function PostList(props) {
+    const { posts, onClickItem } = props;
+
+    return (
+        <Wrapper>
+            {/* posts 배열을 map 함수를 사용하여 순회합니다. */}
+            {posts.map((post) => {
+                return (
+                    <PostListItem
+                        key={post.id} // 각 PostListItem 컴포넌트에 key로 post의 id를 할당합니다.
+                        post={post} // 현재 순회 중인 post를 PostListItem 컴포넌트에 전달합니다.
+                        onClick={() => {
+                            onClickItem(post); // 클릭 이벤트 발생 시 onClickItem 함수를 호출하고, post를 인자로 전달합니다.
+                        }}
+                    />
+                );
+            })}
+        </Wrapper>
+    );
+}
+
+export default Button;
