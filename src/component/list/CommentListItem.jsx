71d806aa71aba9de4fcad9619d14e077d1729e4a
ptokos <jiseokchoij@gmail.com>
2023-05-25 15:14:09 +0900

    feat: 개별 댓글에 해당하는 CommentListItem 컴포넌트 구현

---
+++ src/component/list/CommentListItem.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    width: calc(100% - 32px);
+    padding: 8px 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border: 1px solid grey;
+    border-radius: 8px;
+    cursor: pointer;
+    background: white;
+    :hover {
+        background: lightgrey;
+    }
+`;
+
+const contentText = styled.p`
+    font-size: 16px;
+    white-space: pre-wrap;
+`;
+
+function CommentListItem(props) {
+    const { comment } = props;
+
+    return (
+        <Wrapper>
+            <contentText>{comment.content}</contentText>
+        </Wrapper>
+    );
+}
+
+export default CommentListItem;
