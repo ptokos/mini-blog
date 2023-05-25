02b40cf9419ea2684b546e8ca7faefafb400d217
ptokos <jiseokchoij@gmail.com>
2023-05-25 14:34:33 +0900

    feat: 버튼 컴포넌트

---
+++ src/component/ui/Button.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+// styled-components를 import합니다.
+
+const styledButton = styled.button`
+    /* 버튼 스타일을 정의합니다. */
+    padding: 8px 16px;
+    font-size: 16px;
+    border-width: 1px;
+    boder-radius: 8px;
+    cursor: pointer;
+    margin-bottom: 8px;
+`;
+
+function Button(props) {
+    // props로부터 title과 onClick을 추출합니다.
+    const { title, onClick } = props;
+
+    // styledButton를 사용하여 버튼을 렌더링합니다.
+    return <styledButton onClick={onClick}>{title || 'button'}</styledButton>;
+}
+
+export default Button;
