00d5a39cac3ffc11315513549d1a5ec4e7f96b3b
ptokos <jiseokchoij@gmail.com>
2023-05-25 15:52:17 +0900

    라우팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
+import MainPage from './component/page/MainPage';
+
+const MainTitleText = styled.p `
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainTitleText> ptokos의 미니 블로그 </MainTitleText>
+      <Routes>
+        <Route index element={<MainPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
